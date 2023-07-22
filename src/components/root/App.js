9d4a919e633cb44d49cd4dea91d1101c1329333b
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-07-22 18:25:58 +0300

    Navi and Dashboard moved into container

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,9 +1,13 @@
+import {Container} from 'reactstrap';
 import Navi from '../navi/Navi';
+import Dashboard from './Dashboard';
+
 function App () {
   return (
-    <div>
+    <Container>
       <Navi />
-    </div>
+      <Dashboard />
+    </Container>
   );
 }
 
