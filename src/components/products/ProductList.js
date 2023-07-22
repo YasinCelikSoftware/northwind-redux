6bb266bf470f649411f9f5dd1bf9582fd193c1af
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-07-23 01:22:04 +0300

    Mini Trial --will change

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class ProductList extends Component {
+class ProductList extends Component {
   render () {
     return (
       <div>
         <h3>
-          ProductList
+          {this.props.currentCategory.categoryName}
         </h3>
       </div>
     );
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    currentCategory: state.changeCategoryReducer,
+  };
+}
+
+function mapDispatchToProps (dispatch) {}
+
+export default connect (mapStateToProps, mapDispatchToProps) (ProductList);
