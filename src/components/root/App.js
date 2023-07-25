54344b5e8c1e7f1a409c510fa223cce88750d93a
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-07-26 00:58:56 +0300

    saveproduct routes added

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -3,7 +3,8 @@ import Navi from '../navi/Navi';
 import Dashboard from './Dashboard';
 import {Route, Routes} from 'react-router-dom';
 import CartDetail from '../cart/CartDetail';
-import NotFound from './NotFound';
+import NotFound from '../common/NotFound';
+import AddOrUpdateProduct from '../products/AddOrUpdateProduct';
 import '../../css/App.css';
 
 function App () {
@@ -14,7 +15,13 @@ function App () {
         <Route path="/" exact element={<Dashboard />} />
         <Route path="/products" exact element={<Dashboard />} />
         <Route path="/cart" exact element={<CartDetail />} />
+        <Route path="/saveproduct" element={<AddOrUpdateProduct />} />
+        <Route
+          path="/saveproduct/:productId"
+          element={<AddOrUpdateProduct />}
+        />
         <Route path="*" element={<NotFound />} />
+
       </Routes>
 
     </Container>
