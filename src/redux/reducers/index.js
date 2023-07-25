35f2a84f239d452b025521c81e8867a236964859
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-07-25 17:42:46 +0300

    unneccessary import removed, new reducer added

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,13 +3,14 @@ import changeCategoryReducer from './changeCategoryReducer';
 import categoryListReducer from './categoryListReducer';
 import productListReducer from './productListReducer';
 import cartReducer from './cartReducer';
-import 'alertifyjs/build/css/alertify.min.css';
+import saveProductReducer from './saveProductReducer';
 
 const rootReducer = combineReducers ({
   changeCategoryReducer,
   categoryListReducer,
   productListReducer,
   cartReducer,
+  saveProductReducer,
 });
 
 export default rootReducer;
