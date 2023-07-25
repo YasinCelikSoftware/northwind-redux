6e6b9a149dbb11db41eb95f0adbcf04169ddd03c
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-07-25 13:12:58 +0300

    Router usage

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,12 +1,21 @@
 import {Container} from 'reactstrap';
 import Navi from '../navi/Navi';
 import Dashboard from './Dashboard';
+import {Route, Routes} from 'react-router-dom';
+import CartDetail from '../cart/CartDetail';
+import NotFound from './NotFound';
 
 function App () {
   return (
     <Container>
       <Navi />
-      <Dashboard />
+      <Routes>
+        <Route path="/" exact element={<Dashboard />} />
+        <Route path="/products" exact element={<Dashboard />} />
+        <Route path="/cart" exact element={<CartDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
     </Container>
   );
 }
